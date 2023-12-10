1cb5b17e7cc87f5c00530018492fb0c2a3d2cdcc
WebDevelopo11 <si.sada93@gmail.com>
2023-12-10 16:53:03 +0100

    config color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,30 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#000112',
+      'background': {
+        light: '#F4F7FD',
+        dark: '#20212C',
+      },
+      'darkGrey': '#2B2C37',
+      'lines': {
+        light: '#E4EBFA',
+        dark: '#3E3F4E',
+      },
+      'grey': '#828FA3',
+      'white': '#FFFFFF',
+      'purple': {
+        DEFAULT: '#635FC7',
+        hover: '#A8A4FF',
+      },
+      'red': {
+        DEFAULT: '#EA5555',
+        hover: '#FF9898',
+      },
+    },
     extend: {},
   },
   plugins: [],
